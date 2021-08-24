89f6105fe7fcd8832ba4a7b66745c8ef4bb2404f
taebinkim12 <taebinkim12@gmail.com>
2021-08-24 11:14:11 -0400

    Receive input from user

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask("SuperScrapper")
 
 @app.route("/")
 def home():
-    return "Hello and Welcome!"
+    return render_template("home.html")
 
 @app.route("/test")                                 # "@" --> Decorator: looks for a function to execute underneath
 def test():
     return "contact me!"
+
+@app.route("/report")
+def report():
+    word = (request.args.get('word'))                 # Comes as Dictionary
+    return render_template("report.html", searchBy= word)
+
+# @app.route("/<username>")                           # Dynamic URLs
+# def username(username):
+#     return f"Hello your name is {username}"
+
 
 app.run()

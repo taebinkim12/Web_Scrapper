af4ad004fd60f44dc13fcb3897289e33f5ae0bdd
taebinkim12 <taebinkim12@gmail.com>
2021-08-25 09:57:03 -0400

    Bring the actual scrapper

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask("SuperScrapper")
 
@@ -12,8 +12,12 @@ def test():
 
 @app.route("/report")
 def report():
-    word = (request.args.get('word'))                 # Comes as Dictionary
-    return render_template("report.html", searchBy= word)
+    word = request.args.get('word')                 # Comes as Dictionary
+    if word:
+        word = word.lower()
+    else:
+        return redirect("/")
+    return render_template("report.html", searchBy = word)
 
 # @app.route("/<username>")                           # Dynamic URLs
 # def username(username):
